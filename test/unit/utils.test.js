e15b49e358a38b6cf5c4418a25264229ce9b62fa
dk4g <157156245+dk4g@users.noreply.github.com>
2024-10-11 12:50:21 +0100

    add unit test for formatDate method

--- test/unit/utils.test.js
+++ test/unit/utils.test.js
@@ -1,4 +1,4 @@
-const { getLabel } = require('../../utils');
+const { getLabel, formatDate } = require('../../utils');
 
 describe('Utilities \'getLabel\'', () => {
   test('returns undefined when an unexpected fieldKey parameter is passed', () => {
@@ -11,5 +11,19 @@ describe('Utilities \'getLabel\'', () => {
     expect(getLabel('satisfaction', 'very-cheese')).toBe(undefined);
     expect(getLabel('satisfaction', null)).toBe(undefined);
     expect(getLabel('satisfaction', undefined)).toBe(undefined);
+  });
+});
+
+describe('Utilities \'formatDate\'', () => {
+  test('reformats a string type date from YYYY-MM-DD to DD MM YYYY', () => {
+    expect(formatDate('1987-08-14')).toBe('14 08 1987');
+  });
+
+  test('reformats a string type date from MM/DD/YYYY to DD MM YYYY', () => {
+    expect(formatDate('08/14/1987')).toBe('14 08 1987');
+  });
+
+  test('throws an error when the parameter cannot be parsed as a date', () => {
+    expect(() => formatDate('hello')).toThrow();
   });
 });
