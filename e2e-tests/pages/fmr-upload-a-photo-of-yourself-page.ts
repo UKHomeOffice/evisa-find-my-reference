48b189e7e1acb1ca735b3ae0290946adf0f65a1e
GrahamOsifoHO <Graham.Osifo@digital.homeoffice.gov.uk>
2026-07-21 15:18:04 +0100

    update test

--- e2e-tests/pages/fmr-upload-a-photo-of-yourself-page.ts
+++ e2e-tests/pages/fmr-upload-a-photo-of-yourself-page.ts
@@ -5,7 +5,6 @@ import * as path from 'path';
 export class fmrUploadAPhotoOfYourselfPage extends basePage {
   readonly fileUpload: Locator;
   readonly uploadForm: Locator;
-  // readonly fileUploadInput: Locator;
   readonly continueWithoutUploadButton: Locator;
   readonly uploadedDocumentsHeading: Locator;
   readonly fileUploadErrorMaxFileSize: Locator;
@@ -15,7 +14,6 @@ export class fmrUploadAPhotoOfYourselfPage extends basePage {
     super(page);
     this.fileUpload = page.locator("input[type='file']");
     this.uploadForm = page.locator("form[name='file-upload-form']");
-    // this.fileUploadInput = page.locator('#file-upload');
     this.continueWithoutUploadButton = page.locator("[name='continueWithoutUpload']");
     this.uploadedDocumentsHeading = page.getByRole('heading', { name: /You have uploaded/i });
     this.fileUploadErrorMaxFileSize = page.locator('#file-upload-error-maxFileSize');
@@ -36,7 +34,6 @@ export class fmrUploadAPhotoOfYourselfPage extends basePage {
 
   async uploadFileFromUserUploadFolder(fileName: string) {
     await this.fileUpload.setInputFiles(this.getUploadFilePath(fileName));
-    await this.uploadForm.evaluate((form: HTMLFormElement) => form.submit());
     await this.page.waitForLoadState('domcontentloaded');
   }
 
@@ -55,8 +52,6 @@ export class fmrUploadAPhotoOfYourselfPage extends basePage {
   async completeUploadAPhotoOfYourselfPage(fileName: string) {
     await this.assertPageTitle(this.page, await this.expectedPageTitle());
     await this.uploadFileFromUserUploadFolder(fileName);
-    await this.page.waitForLoadState('domcontentloaded');
-    await expect(this.uploadedDocumentsHeading).toContainText('You have uploaded', { timeout: 15_000 });
     await this.selectContinueUploadButton();
   }
 }
